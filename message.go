719a753a5e8ee3cafe121cc29b827ffccd9c5495
jsx7ba <jscheiber@gmail.com>
2024-06-02 19:00:28 -0400

    use the clover id

--- message.go
+++ message.go
@@ -6,10 +6,10 @@ import (
 
 // ChatMessage A message that the user sends to a room.
 type ChatMessage struct {
-	Id     string    `clover:"id" json:"id"`
+	Id     string    `clover:"_id" json:"id"`
 	Date   time.Time `clover:"date" json:"date"`
 	RoomId string    `clover:"roomId" json:"roomId"`
 	Sender string    `clover:"sender" json:"sender"`
 	Text   string    `clover:"text" json:"text"`
-	Images []Image   `clover:"text" json:"images"`
+	Images []Image   `clover:"images" json:"images"`
 }
