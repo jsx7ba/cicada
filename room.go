b5d09fad41cccf2ef9593d0ff1e3882fd6fbee5f
jsx7ba <jscheiber@gmail.com>
2024-06-02 19:00:59 -0400

    add description to the room

--- room.go
+++ room.go
@@ -2,7 +2,8 @@ package cicada
 
 // Room indicates who can communicate with each other.
 type Room struct {
-	Id      string   `clover:"id" json:"id,omitempty"`
-	Name    string   `clover:"name" json:"name"`
-	Members []string `clover:"members" json:"members,omitempty"`
+	Id          string   `clover:"id" json:"id,omitempty"`
+	Name        string   `clover:"name" json:"name"`
+	Description string   `clover:"description" json:"description"`
+	Members     []string `clover:"members" json:"members,omitempty"`
 }
